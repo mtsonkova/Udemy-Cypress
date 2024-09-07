ce6a5d98fc50a20adc3225cb4969c811b346bd55
mtsonkova <mtsonkova950@gmail.com>
2024-09-08 00:08:50 +0300

    In progress'

--- cypress/integration/demo_tests/Test1.js
+++ cypress/integration/demo_tests/Test1.js
@@ -1,8 +1,16 @@
+/// <reference types="Cypress" />
 const url = 'https://rahulshettyacademy.com/seleniumPractise/#/';
 
 describe('GreenKart TestSuite', function() {
-    it('Navigate to GreenKartUrl', async() => {
+    it('Navigate to GreenKartUrl', () => {
         cy.visit(url);
+        cy.get('.search-keyword')
+        .type('ca');
+        cy.wait(2000);
+        //In Cypress get acts as findElement of Selenium
+        
+        //cy.get('.products:visible').should('have.length', 4);
+        cy.get('div.products').find('.product').should('have.length', 4);
       
     })
 })
