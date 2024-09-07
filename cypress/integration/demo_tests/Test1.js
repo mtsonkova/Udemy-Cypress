0df83f4e0565e17d77a1e3cfbd52d2ff2469bbb9
mtsonkova <mtsonkova950@gmail.com>
2024-09-07 22:54:32 +0300

    Project started

---
+++ cypress/integration/demo_tests/Test1.js
@@ -0,0 +1,8 @@
+const url = 'https://rahulshettyacademy.com/seleniumPractise/#/';
+
+describe('GreenKart TestSuite', function() {
+    it('Navigate to GreenKartUrl', async() => {
+        cy.visit(url);
+        expect(cy.url).should(equal, url);
+    })
+})
