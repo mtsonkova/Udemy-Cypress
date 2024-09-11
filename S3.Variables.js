3da7b6feb8d7e406a3da8b9c9bf8ca626d21ccb2
mtsonkova <mtsonkova950@gmail.com>
2024-09-11 14:30:59 +0300

    In progress

---
+++ S3.Variables.js
@@ -0,0 +1,10 @@
+let name = "Jane Dow";
+let age;
+age = 12;
+age = 47;
+
+const userName = 'user1'; // constant variables must always be declared
+
+// constant variable cannot be reasinged. This row will through an error
+//userName = 'user5'; 
+console.log(userName);
