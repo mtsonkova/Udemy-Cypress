eb6abf5446eec3ffa12bf9c453203d779cf30f74
mtsonkova <mtsonkova950@gmail.com>
2024-09-13 23:05:05 +0300

    In progress

--- Functions.js
+++ Functions.js
@@ -18,3 +18,28 @@ addNumbers(2, 4, 5, 2, 0, 12);
 () => {
     console.log('Hi from and arrow function');
 } 
+
+
+//Callback function -> a function passed as an argument to another function
+
+function printMsg(msg) {
+    console.log(`Calling the callback funcion...`);
+    console.log(msg);
+}
+
+function greet(name, greeter) {
+console.log(`Calling the main function...`);
+let greetMsg = `Hello, ${name}`;
+return greeter(greetMsg);
+}
+
+
+let res = greet('Rob', (res) => {
+    return `the received value from the main function: ${res}`
+});
+console.log(res);
+
+
+// if a function does not return anything you cannot save it in a variable;
+
+// if a callback function uses a return statement, that will be returned to the main function
