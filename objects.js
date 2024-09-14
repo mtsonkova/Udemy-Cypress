0de077490b57c4dde80d91a8f66dbe83ef78ba4d
mtsonkova <mtsonkova950@gmail.com>
2024-09-14 17:19:32 +0300

    In progress

--- objects.js
+++ objects.js
@@ -31,3 +31,47 @@ console.log(reportData.macID);
 console.log(reportData.envFile);
 console.log(reportData.test_config);
 
+
+console.log(reportData["Pass/Fail flag"]);
+
+// Access object keys
+let keys = Object.keys(reportData);
+
+let values = Object.values(reportData);
+
+let entries = Object.entries(reportData);
+
+
+// Object.entries(reportData).forEach(([key, value]) => {
+//     console.log(`${key}: ${value}`);
+//   });  
+
+
+  // Merge 2 objects
+
+  let person = {
+    firstName: 'Simon',
+    lastName: 'Hill',
+    age: 24
+  }
+
+  let student = {
+    university: 'SoftUni',
+    program: 'Softare engineering',
+    facultiNumber: '123432',
+    courses: ['Web development', 'JavaScript advanced', 'SQL'],
+  }
+
+  //First way with Object.assign
+
+  let trainee = Object.assign(person, student);
+  //console.log(trainee);
+  
+  //Second way with ...rest operator
+
+  let trainee2 = {
+    ...person,
+    ...student
+  } 
+
+  console.log(trainee2);
